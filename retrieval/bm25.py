c26c288260e7ab1dd2765bdc2d6068000f0377ce
Elaine Farrow <Elaine.Farrow@ed.ac.uk>
2017-12-19 19:40:31 +0000

    make the script generate the full JSON file add case folding

--- retrieval/bm25.py
+++ retrieval/bm25.py
@@ -1,8 +1,50 @@
 
-with open ("titels.txt") as f:
-    for line in f:
-        print ("#combine(",end='')
-        for word in line.split():
-            print("#bm25(" + word + ") ",end='')
-        print (")")
+import sys
 
+def printUsageAndExit():
+    args = " input_file"
+    print("Usage: " + sys.argv[0] + args, file=sys.stderr)
+    sys.exit(1)
+
+def normalise(word):
+    # special cases
+    if word in ('IoT', 'AI'):
+        return word
+    # casefold the rest
+    return word.lower()
+
+def makeQuery(num, line):
+    terms = ['#bm25({})'.format(normalise(word)) for word in line.split()]
+    query = '    {\n'
+    query += '      "number" : "{}",\n'.format(num+1)
+    query += '      "text"   : "#combine({})"\n'.format(' '.join(terms))
+    query += '    }'
+    return query
+
+def makeQueries(filename):
+    with open (filename) as f:
+        print(',\n'.join([makeQuery(num, line) for num, line in enumerate(f)]))
+
+def main():
+    if len(sys.argv) < 2:
+        printUsageAndExit()
+    inputfilename = sys.argv[1]
+    print("""{
+  "casefold": true,
+  "fields": [
+    "text",
+    "headline",
+    "title",
+    "head",
+    "leadpara"
+  ],
+  "index": "../idx/tedDirector_en.idx",
+  "verbose": true,
+  "requested": 1000,
+  "queries"  : [""")
+    makeQueries(inputfilename)
+    print(' ]\n}')
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
