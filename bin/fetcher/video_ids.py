c9236d9a20e5ceaf803fb944a9b95187658f3f62
goweiting <gwt.543@gmail.com>
2017-12-20 09:07:51 +0000

    fetcher scripts update to reflect fetching tedEd,tedX,tedDir

--- bin/fetcher/video_ids.py
+++ bin/fetcher/video_ids.py
@@ -19,62 +19,77 @@ API_VERSION = 'v3'
 
 
 def get_authenticated_service():
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE,
-                                                     SCOPES)
-    credentials = flow.run_console()
-    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE,
+                                                   SCOPES)
+  credentials = flow.run_console()
+  return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
 
 def playlist_items_list_by_playlist_id(client, **kwargs):
-    response = client.playlistItems().list(**kwargs).execute()
-    if not response.get('items'):
-        return [], None
+  response = client.playlistItems().list(**kwargs).execute()
+  if not response.get('items'):
+    return [], None
 
-    videoIds = []
-    for item in response['items']:
-        videoIds.append(str(item['contentDetails']['videoId']))
-    if response.get('nextPageToken'):
-        return videoIds, response['nextPageToken']
-    else:
-        return videoIds, None
+  videoIds = []
+  for item in response['items']:
+    videoIds.append(str(item['contentDetails']['videoId']))
+  if response.get('nextPageToken'):
+    return videoIds, response['nextPageToken']
+  else:
+    return videoIds, None
 
 
 def fetch_playlist_items(client, playlistId):
-	"""Refer to the following SO answer for how to retrieve a list
-	of uploaded video Ids for a given channel
-	:see https://stackoverflow.com/a/13504900/4103546
-	"""
-    results = []
-    nextPageToken = None
+  """Refer to the following SO answer for how to retrieve a list
+  of uploaded video Ids for a given channel
+  :see https://stackoverflow.com/a/13504900/4103546
+  """
+  results = []
+  nextPageToken = None
+  with open('res/tedX_vid_list.txt', 'w') as fout:
     while True:
-        kwargs = {
-            'part': 'contentDetails',
-            'maxResults': 50,
-            'playlistId': 'UUAuUUnT6oDeKwE6v1NGQxug'
-        }
-        if nextPageToken:
-            kwargs['pageToken'] = nextPageToken
-        videoIds, nextPageToken = playlist_items_list_by_playlist_id(client,
-                                                                     **kwargs)
-        results.extend(videoIds)
-        if __debug__:
-            print("no. of videos: %s, next page token: %s"
-                  % (len(videoIds), nextPageToken))
-        if not nextPageToken:
-            break
-    return results
+      # kwargs = { # TED-DIRECTOR
+      #     'part': 'contentDetails',
+      #     'maxResults': 50,
+      #     'playlistId': 'UUAuUUnT6oDeKwE6v1NGQxug'
+      # }
+      # kwargs = { # TED-ED
+      #     'part': 'contentDetails',
+      #     'maxResults': 50,
+      #     'playlistId': 'UUsooa4yRKGN_zEE8iknghZA'
+      # }
+      kwargs = { # TEDx
+          'part': 'contentDetails',
+          'maxResults': 50,
+          'playlistId': 'UUsT0YIqwnpJCM-mx7-gSA4Q'
+      }
+      if nextPageToken:
+        kwargs['pageToken'] = nextPageToken
+      videoIds, nextPageToken = playlist_items_list_by_playlist_id(client,
+                                                                   **kwargs)
+      fout.write('\n'.join(videoIds))
+
+      # results.extend(videoIds)
+      if __debug__:
+        print("no. of videos: %s, next page token: %s"
+              % (len(videoIds), nextPageToken))
+      if not nextPageToken:
+        break
+  return results
 
 
 def main():
-    service = get_authenticated_service()
-    # following playlist Id is for TEDtalksDirector's uploaded videos
-    videoIds = fetch_playlist_items(service,
-                                    playlistId='UUAuUUnT6oDeKwE6v1NGQxug')
-    with open('uploaded_videos.txt', 'w') as fout:
-        fout.write('\n'.join(videoIds))
+  service = get_authenticated_service()
+  # following playlist Id is for TEDtalksDirector's uploaded videos
+  # videoIds = fetch_playlist_items(service,
+  #                                 playlistId='UUAuUUnT6oDeKwE6v1NGQxug')
+  # videoIds = fetch_playlist_items(service, # TED-ED
+  #                                 playlistId='UUsooa4yRKGN_zEE8iknghZA')
+  videoIds = fetch_playlist_items(service,  # TEDx
+                                playlistId='UUsT0YIqwnpJCM-mx7-gSA4Q')
 
 if __name__ == '__main__':
-    # When running locally, disable OAuthlib's HTTPs verification. When
-    # running in production *do not* leave this option enabled.
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-    main()
+  # When running locally, disable OAuthlib's HTTPs verification. When
+  # running in production *do not* leave this option enabled.
+  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+  main()
