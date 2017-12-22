07f7288e3af896603c2006ba34ddf109b9dc4078
Wei Ting GOH <goweiting@users.noreply.github.com>
2017-12-22 23:46:13 +0000

    Ran the queries using galago (#3)
    * newly generated json files for galago retrieval
    
    * oops accidentally deleted pooling.py
    
    * galago results
    
    * remove galago results; pooling will be done with solr's resuts

--- retrieval/query/sdm.py
+++ retrieval/query/sdm.py
@@ -2,47 +2,45 @@
 import re
 import sys
 
+
 def printUsageAndExit():
-    args = " input_file"
-    print("Usage: " + sys.argv[0] + args, file=sys.stderr)
-    sys.exit(1)
+  args = " input_file"
+  print("Usage: " + sys.argv[0] + args, file=sys.stderr)
+  sys.exit(1)
+
 
 def makeQuery(num, line):
-    # remove punctuation
-    line = re.sub('\W', ' ', line)
-    terms = [word for word in line.split()]
-    query = '    {\n'
-    query += '      "number": "{}",\n'.format(num+1)
-    query += '      "scorer" : "bm25",\n'
-    query += '      "text": "{}"\n'.format(' '.join(terms))
-    query += '    }'
-    return query
+  # remove punctuation
+  line = re.sub('\W', ' ', line)
+  terms = [word for word in line.split()]
+  query = '    {\n'
+  query += '      "number": "{}",\n'.format(num + 1)
+  query += '      "scorer" : "bm25",\n'
+  query += '      "text": "{}"\n'.format(' '.join(terms))
+  query += '    }'
+  return query
+
 
 def makeQueries(filename):
-    with open (filename) as f:
-        print(',\n'.join([makeQuery(num, line) for num, line in enumerate(f)]))
+  with open(filename) as f:
+    print(',\n'.join([makeQuery(num, line) for num, line in enumerate(f)]))
+
 
 def main():
-    if len(sys.argv) < 2:
-        printUsageAndExit()
-    inputfilename = sys.argv[1]
-    print("""{
+  if len(sys.argv) < 2:
+    printUsageAndExit()
+  inputfilename = sys.argv[1]
+  print("""{
   "casefold": true,
   "verbose": true,
-  "fields": [
-    "text",
-    "headline",
-    "title",
-    "head",
-    "leadpara"
-  ],
-  "index": ["../idx/tedDirector_en.idx", "../idx/tedX_en-ALL.idx", "../idx/tedEd_en-ALL.idx"],
+  "index": ["../../idx/galago/ted.en.idx"],
   "requested": 1000,
   "operatorWrap": "sdm",
   "queries": [""")
-    makeQueries(inputfilename)
-    print('  ]\n}')
+  makeQueries(inputfilename)
+  print('  ]\n}')
+
 
 if __name__ == "__main__":
-    # execute only if run as a script
-    main()
+  # execute only if run as a script
+  main()
